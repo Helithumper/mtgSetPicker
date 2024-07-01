b651f081465f8c8f9da2d2653f2c63c7cc2bf269
James Krepelka <709914+Jooms@users.noreply.github.com>
2024-07-01 08:15:06 -0700

    Filter out digital cards

--- scryfallAPI.py
+++ scryfallAPI.py
@@ -1,17 +1,22 @@
 import requests
 import time
 
+
 def search_card(name: str) -> dict:
     # Add ratelimit to be nice
     # From: https://scryfall.com/docs/api#rate-limits-and-good-citizenship
     time.sleep(0.1)
-    
+
     url = "https://api.scryfall.com/cards/search"
-    params = {"q": name, "unique": "prints"}
+    # Filter out digital cards
+    query_list = [name, "-is:digital"]
+
+    params = {"q": " ".join(query_list), "unique": "prints"}
+
     req = requests.models.PreparedRequest()
     req.prepare_url(url, params)
     response = requests.get(req.url)
 
     if response.status_code == 200:
-        return response.json()    
+        return response.json()
     return None
