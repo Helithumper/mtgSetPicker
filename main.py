de3ca98a81e205b164c43e1308fde730a3c3d5a2
James Krepelka <709914+Jooms@users.noreply.github.com>
2024-06-30 21:40:36 -0700

    Clean up card data capture

--- main.py
+++ main.py
@@ -45,40 +45,67 @@ def process_list(names: list) -> list:
             
     return list_data
 
+class card:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
 class card_set:
-    def __init__(self):
+    def __init__(self, name, set_type):
+        self.name = name
+        self.set_type = set_type
         self.count = 0
         self.cards = []
 
-    def add(self, name):
+    def add(self, card):
         self.count += 1
-        self.cards.append(name)
+        self.cards.append(card)
 
 def get_set_counts(card_list: dict) -> dict:
     set_map = {}
-    for card in card_list:
-        for card_data in card['data']:
+    for c in card_list:
+        for card_data in c['data']:
             set_id = card_data['set_id']
             if set_id not in set_map.keys():
-                set_map[set_id] = card_set()
-            set_map[set_id].add(card_data['name'])
+                set_map[set_id] = card_set(
+                    card_data['set_name'],
+                    card_data['set_type']
+                )
+            set_map[set_id].add( card(
+                card_data['name'],
+                card_data['prices']['usd'],
+            ))
     return set_map
 
 def card_set_count(cs: card_set) -> int:
     return cs[1].count
 
 def sort_set_counts(set_counts: dict) -> dict:
+    sorted_set = {}
     for k, v in sorted(set_counts.items(), key=card_set_count, reverse=True):
+        sorted_set[k] = v
+    return sorted_set
+
+def print_sorted_set(set_counts: dict, top_n: int) -> dict:
+    i = 0
+    for k, v in set_counts.items():
         print("k:", k)
-        print("v:", v.cards)
+        print("\tname:", v.name)
+        print("\tset_type:", v.set_type)
+        print("\tcards:")
+        for c in v.cards:
+            print("\t\tName:", c.name, "- Price:", c.price)
+        i += 1
+        if i >= top_n:
+            break
 
 cards = ["Ratcatcher", "Swarm of Rats", "Jet Medallion"]
 
 data = process_list(cards)
-
 set_counts = get_set_counts(data)
+set_counts = sort_set_counts(set_counts)
 
-print(sort_set_counts(set_counts))
+print_sorted_set(set_counts, 5)
 
 # sets = []
 # for c in data['data']:
